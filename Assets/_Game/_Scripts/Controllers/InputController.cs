700588716e7117c2a306472228e134b0bd4ff654
Enes <yslyrt.enes@gmail.com>
2022-04-23 18:26:01 +0300

    Code refactor

--- Assets/_Game/_Scripts/Controllers/InputController.cs
+++ Assets/_Game/_Scripts/Controllers/InputController.cs
@@ -2,21 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Helpers;
+using System;
 
 public class InputController : MonoSingleton<InputController>
 {
-    [SerializeField]
-    private Vector2 minPower;
-
-    [SerializeField]
-    private Vector2 maxPower;
-
-
-    private Vector2 force;
-    private Vector2 startPoint;
-    private Vector2 direction;
-
-    public Vector2 Force => force;
+    public event Action<TouchPhase,Vector2> Clicked;
 
     private void Update() 
     {
@@ -29,24 +19,7 @@ public class InputController : MonoSingleton<InputController>
         {
             Touch touch = Input.GetTouch(0);
 
-            switch (touch.phase)
-            {
-                case TouchPhase.Began:
-                    startPoint = touch.position;
-                    break;
-                case TouchPhase.Moved:
-                    direction = touch.position - startPoint;
-                    break;
-                case TouchPhase.Ended:
-                    force = new Vector2
-                    (
-                        Mathf.Clamp((direction.x)*-1, minPower.x, maxPower.x),
-                        Mathf.Clamp((direction.y)*-1, minPower.y, maxPower.y)
-                    );
-                    break;
-            }
-            
-
+            Clicked?.Invoke(touch.phase, Camera.main.ScreenToWorldPoint(touch.position));
         }
     }
 }
--- Assets/_Game/_Scripts/Entities/Bird.cs
+++ Assets/_Game/_Scripts/Entities/Bird.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [SerializeField]
-    private float power;
+    private Vector2 minPower;
+
+    [SerializeField]
+    private Vector2 maxPower;
+
+    [SerializeField]
+    private float force;
+
     private Rigidbody2D rb;
+
+    private Vector2 startTouchPosition;
 
     private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
+        InputController.Instance.Clicked += OnClicked;
     }
 
-    private void Update()
+    private void OnClicked(TouchPhase phase, Vector2 touchPosition)
     {
-        rb.AddForce(InputController.Instance.Force * power, ForceMode2D.Impulse);
+        switch (phase)
+        {
+            case TouchPhase.Began:
+                startTouchPosition = touchPosition;
+                break;
+            case TouchPhase.Moved:
+                transform.position = touchPosition;
+                break;
+            case TouchPhase.Stationary:
+                transform.position = touchPosition;
+                break;
+            case TouchPhase.Ended:
+                rb.AddForce((startTouchPosition - touchPosition).normalized * force);
+                InputController.Instance.Clicked -= OnClicked;
+                break;
+        }
     }
 }
