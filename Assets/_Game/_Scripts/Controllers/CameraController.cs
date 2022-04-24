24cd08fd8f6fd33f8d34218a547fff118ea6b23c
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-04-24 20:12:26 +0300

    Camera controller added

---
+++ Assets/_Game/_Scripts/Controllers/CameraController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    [SerializeField]
+    private GameObject bird;
+    
+
+    private void FixedUpdate() 
+    {
+        CameraMovement();
+    }
+    private void CameraMovement()
+    {
+        if (bird.transform.position.x >= 0)
+        {
+            //mainCamera.transform.position = new Vector3(bird.transform.position.x,0,-10);
+            mainCamera.transform.position = Vector3.Lerp(new Vector3(mainCamera.transform.position.x,4.33f,-10),new Vector3(bird.transform.position.x,4.33f,-10),0.125f);
+        }
+    }
+}
