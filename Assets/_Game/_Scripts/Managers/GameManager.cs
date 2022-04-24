93d5685edfaae67ec14ef7742c39ee5d97cd5bea
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-04-24 20:13:17 +0300

    GameManager added

---
+++ Assets/_Game/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Helpers;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public event Action<GameStates> GameStateChanged;
+
+    public GameStates GameState => gameState;
+
+    private GameStates gameState;
+
+    private void Start() 
+    {
+        UpdateGameState(GameStates.Game);    
+    }
+
+    public void UpdateGameState(GameStates newState)
+    {
+        gameState = newState;
+
+        GameStateChanged?.Invoke(newState);
+    }
+}
+
