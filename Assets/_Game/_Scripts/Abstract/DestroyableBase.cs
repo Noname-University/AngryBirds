6452911db22f66984bb32eecb6b4c7d25ba3e9b8
keremtpl <keremtpl57@gmail.com>
2022-04-25 23:20:26 +0300

    score system added

--- Assets/_Game/_Scripts/Abstract/DestroyableBase.cs
+++ Assets/_Game/_Scripts/Abstract/DestroyableBase.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class DestroyableBase : MonoBehaviour,IDestroyable
+public abstract class DestroyableBase : MonoBehaviour, IDestroyable
 {
     [SerializeField]
     protected float health;
+    [SerializeField]
+    protected int scoreValue;
+
+
 
     public virtual void Destroy()
     {
         Destroy(gameObject);
+        ScoreController.Instance.IncreaseScore(scoreValue);
+
+
     }
 
     public virtual void Hit(float damage)
     {
         health -= damage;
-        
+
         if (health <= 0)
         {
             Destroy();
---
+++ Assets/_Game/_Scripts/Controllers/ScoreController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Helpers;
+using TMPro;
+
+public class ScoreController : MonoSingleton<ScoreController>
+{
+    private int currentScore;
+
+
+    public void IncreaseScore(int scoreValue)
+    {
+        currentScore += scoreValue;
+        Debug.Log(currentScore);
+
+    }
+
+
+}
