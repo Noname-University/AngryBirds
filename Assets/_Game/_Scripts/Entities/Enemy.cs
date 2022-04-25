b9123e5ceaf89201f0c0aa34d911478279b3264e
keremtpl <keremtpl57@gmail.com>
2022-04-25 23:22:17 +0300

    Enemy manager added and enemy counter added

--- Assets/_Game/_Scripts/Entities/Enemy.cs
+++ Assets/_Game/_Scripts/Entities/Enemy.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 
 public class Enemy : DestroyableBase
 {
+    private void Start()
+    {
+        EnemyManager.Instance.EnemyCount = 1;
+
+    }
+
+    public override void Destroy()
+    {
+        base.Destroy();
+        EnemyManager.Instance.EnemyCount = -1;
+    }
 }
---
+++ Assets/_Game/_Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Helpers;
+using System;
+
+public class EnemyManager : MonoSingleton<EnemyManager>
+{
+    private int enemyCount;
+    public int EnemyCount
+    {
+        get => enemyCount;
+
+        set
+        {
+
+            enemyCount += value;
+            Debug.Log(enemyCount);
+            if (EnemyCount == 0)
+            {
+                GameManager.Instance.UpdateGameState(GameStates.Success);
+            }
+        }
+    }
+}
