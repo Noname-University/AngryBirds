08772fae8c1898d715d133e86a4b134c9c2e3e61
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-04-27 22:02:09 +0300

    code refactor

--- Assets/_Game/_Scripts/Entities/ReClickableBird.cs
+++ Assets/_Game/_Scripts/Entities/ReClickableBird.cs
@@ -6,11 +6,36 @@ public class ReClickableBird : Bird
 {
     public override void OnClicked(TouchPhase phase, Vector2 touchPosition)
     {
-        base.OnClicked(phase, touchPosition);
-        if (phase == TouchPhase.Ended)
-        {
-            GameManager.Instance.UpdateGameState(GameStates.ReClickable);
-        }
+            if (!isTouchBird)
+            {
+                base.OnClicked(phase, touchPosition);
+                if(phase == TouchPhase.Ended)
+                {
+                    GameManager.Instance.UpdateGameState(GameStates.ReClickable);
+                }
+            }
+            else
+            {
+                if (phase == TouchPhase.Began)
+                {
+                     trajectoryPrediction.enabled = false;
+                }
+                else if(phase == TouchPhase.Moved)
+                {
+                    return;
+                }
+                else if(phase == TouchPhase.Stationary)
+                {
+                    return;
+                }
+                else if(phase == TouchPhase.Ended)
+                {
+                    rb.velocity *= 3.5f;
+                    isTouchBird=false;
+                    InputController.Instance.Clicked -= OnClicked;
+                    GameManager.Instance.UpdateGameState(GameStates.Unclickable);
+                }
+            }
 
-    }
+        }
 }
