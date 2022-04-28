f175192b89892774f2e48fe37b9739de0ba48f67
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-04-28 18:07:18 +0300

    BombBird added.

---
+++ Assets/_Game/_Scripts/Entities/BombBird.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBird : Bird
+{
+    [SerializeField]
+    private float radius;
+
+    [SerializeField]
+    private float explotionForce;
+
+    [SerializeField]
+    private LayerMask layerToHit;
+
+    protected override void OnSecondClickAction(TouchPhase phase, Vector2 touchPosition)
+    {
+        Explode();
+        base.OnSecondClickAction(phase, touchPosition);
+    }
+
+    private void Explode()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,radius,layerToHit);
+        foreach (Collider2D collider in colliders)
+        {
+            Vector2 direction = collider.transform.position - transform.position;
+            collider.GetComponent<Rigidbody2D>().AddForce(direction * explotionForce*Vector2.Distance(transform.position,collider.transform.position));
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,radius);
+    }
+}
