ee1db4336f025122cd0324e56cb28946f059071f
keremtpl <keremtpl57@gmail.com>
2022-05-06 16:03:19 +0300

    Score text added and scene designed

--- Assets/_Game/_Scripts/Controllers/ScoreController.cs
+++ Assets/_Game/_Scripts/Controllers/ScoreController.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Helpers;
-using TMPro;
+using UnityEngine.UI;
 
 public class ScoreController : MonoSingleton<ScoreController>
 {
+    [SerializeField]
+    private Text mainScore;
     private int currentScore;
+
 
 
     public void IncreaseScore(int scoreValue)
     {
         currentScore += scoreValue;
+        mainScore.text = currentScore.ToString();
     }
 
 
